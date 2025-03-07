a618837798c6c80f981eab485d3dc6f3dac96bfb
KaanunAzazel <kaanun.azazel@gmail.com>
2025-03-07 15:12:50 -0300

    STYLE

---
+++ src/config.py
@@ -0,0 +1,6 @@
+# Configurações
+API_KEY = "AIzaSyCtQOZbR1SwjZwqzcLOGnzW-aNVIJbwEGM"
+STREAMERS = {
+    "Hina": "UCQQfneexNE9cX88ZLyD-bQA",
+    "Eeires": "UCYiIgZVotTS9K3eb7nine0g",  # ID do canal do YouTube
+}
--- src/streamer_checker.py
+++ src/streamer_checker.py
@@ -1,11 +1,5 @@
 import requests
-
-# Configurações
-API_KEY = "AIzaSyCtQOZbR1SwjZwqzcLOGnzW-aNVIJbwEGM"
-STREAMERS = {
-    "Hina": "UCQQfneexNE9cX88ZLyD-bQA",
-    "Eeires": "UCYiIgZVotTS9K3eb7nine0g",  # ID do canal do YouTube
-}
+from config import API_KEY, STREAMERS
 
 
 def verificar_streamer_online(channel_id):
